d8e9c21fdf1988bcc01c8f0212685320665c945c
yosrri <yosrri@github.com>
2022-10-02 16:18:01 +0200

    Refactor
    indentation and moved Base url in const val

---
+++ app/src/main/java/com/example/movieappsx/API/APIClient.kt
@@ -0,0 +1,29 @@
+package com.example.movieappsx.API
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val BASE_URL = "https://api.themoviedb.org/3/movie/"
+
+object APIClient {
+    fun getClient(): MovieApi {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor
+            .Level
+            .BODY
+
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(MovieApi::class.java)
+    }
+}
+
