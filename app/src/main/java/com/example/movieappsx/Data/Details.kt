cf35e559d8564b0d624f94da72420915fd85759b
yosrri <yosrri@github.com>
2022-10-03 13:10:17 +0200

    Details Class Parcelable
    Extending the details class to use Parcelable class to be able to send custom object between activities using putExtra

--- app/src/main/java/com/example/movieappsx/Data/Details.kt
+++ app/src/main/java/com/example/movieappsx/Data/Details.kt
@@ -1,19 +1,81 @@
 package com.example.movieappsx.Data
+import android.os.Parcel
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 
 data class Details(
+    @SerializedName("adult")
     val adult: Boolean,
-    val backdrop_path: String,
-    val genre_ids: List<Int>,
-    val id: Int,
-    val original_language: String,
-    val original_title: String,
+    @SerializedName("backdrop_path")
+    val backdropPath: String,
+    @SerializedName("id")
+    var id: Int?,
+    @SerializedName("original_language")
+    val originalLanguage: String,
+    @SerializedName("original_title")
+    val originalTitle: String,
+    @SerializedName("overview")
     val overview: String,
+    @SerializedName("popularity")
     val popularity: Double,
-    val poster_path: String,
-    val release_date: String,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("release_date")
+    val releaseDate: String,
+    @SerializedName("title")
     val title: String,
+    @SerializedName("video")
     val video: Boolean,
-    val vote_average: Double,
-    val vote_count: Int
-)
+    @SerializedName("vote_average")
+    var voteAverage: Double,
+    @SerializedName("vote_count")
+    val voteCount: Int
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readValue(Boolean::class.java.classLoader) as Boolean,
+        parcel.readString().toString(),
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readValue(Double::class.java.classLoader) as Double,
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readValue(Boolean::class.java.classLoader) as Boolean,
+        parcel.readValue(Double::class.java.classLoader) as Double,
+        parcel.readValue(Int::class.java.classLoader) as Int
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeValue(adult)
+        parcel.writeString(backdropPath)
+        parcel.writeValue(id)
+        parcel.writeString(originalLanguage)
+        parcel.writeString(originalTitle)
+        parcel.writeString(overview)
+        parcel.writeValue(popularity)
+        parcel.writeString(posterPath)
+        parcel.writeString(releaseDate)
+        parcel.writeString(title)
+        parcel.writeValue(video)
+        parcel.writeValue(voteAverage)
+        parcel.writeValue(voteCount)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Details> {
+        override fun createFromParcel(parcel: Parcel): Details {
+            return Details(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Details?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
 
