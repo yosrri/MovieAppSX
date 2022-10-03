f2fb652c14b3829fa8921ff98405ecad8c6c3ba1
yosrri <yosrri@github.com>
2022-10-03 13:13:50 +0200

    Sending Object
    Sending custom object not yet finished entirely but tested and working

--- app/src/main/java/com/example/movieappsx/UI/MainActivity.kt
+++ app/src/main/java/com/example/movieappsx/UI/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.movieappsx.UI
 
+import android.content.Intent
 import com.example.movieappsx.Data.Details
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -18,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MovieViewModel
     private lateinit var movieAdapter: MovieAdapter
+    private lateinit var movie: Details
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -31,9 +34,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun prepareRecyclerView() {
+        val intent = Intent(this, MovieDetailsActivity::class.java)
         movieAdapter = MovieAdapter(MovieAdapter.OnClickListener { details ->
-            Toast.makeText(applicationContext, details.title, Toast.LENGTH_SHORT).show()
+//            Toast.makeText(applicationContext, details.title, Toast.LENGTH_SHORT).show()
+            this.movie = details
+//            details.title?.let { Log.d("title", it) }
+//            movie.title.let { Log.d("title", it) }
+            Log.d("Object",movie.title)
+            intent.putExtra("movie", movie)
+            startActivity(intent)
         })
+
+
         binding.activityMainMoviesRv.apply {
             layoutManager = GridLayoutManager(applicationContext, COLUMNS)
             adapter = movieAdapter
